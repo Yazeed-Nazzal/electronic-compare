459a0526383f9e7dbabbab608d5394c66c2b1d20
Yazeed-Nazzal <yazeed_nazal@hotmail.com>
2021-03-28 22:49:02 +0300

    edit admin home page part 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-5">
+<div class="container mt-5 home-container">
     <h1 class="text-center mt-5">Statistics</h1>
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-5 text-center">
         <div class="col-md-3">
-            <p> Total Users</p>
-            <div>
-                <i class="fas fa-users"></i>
+            <h3 class="mb-5"> Total Users</h3>
+            <div class="mb-3">
+                <i class="fas fa-users text-primary"></i>
             </div>
+            <p>{{$usersC}}</p>
 
         </div>
         <div class="col-md-3">
-            <p> Total Category's</p>
-           <div>
-               <i class="fas fa-archive"></i>
+            <h3 class="mb-5"> Total Category's</h3>
+           <div class="mb-3">
+               <i class="fas fa-archive text-primary"></i>
            </div>
+            <p>{{$categoryC}}</p>
 
         </div>
         <div class="col-md-3">
-            <p> Total Items </p>
-            <div>
-                <i class="fas fa-puzzle-piece"></i>
+            <h3 class="mb-5"> Total Items </h3>
+            <div class="mb-3">
+                <i class="fas fa-puzzle-piece text-primary"></i>
             </div>
+            <p>{{$itemC}}</p>
 
         </div>
         <div class="col-md-3">
-            <p> Total Comments </p>
-            <div>
-                <i class="fas fa-comment-dots"></i>
+            <h3 class="mb-5"> Total Comments </h3>
+            <div class="mb-3">
+                <i class="fas fa-comment-dots text-primary"></i>
             </div>
+            <p>{{$commentC}}</p>
 
         </div>
     </div>
