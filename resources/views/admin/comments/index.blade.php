34fbaa18052266de9b01633c3f292c9612f40cc7
Yazeed-Nazzal <yazeed_nazal@hotmail.com>
2021-03-28 21:29:00 +0300

    edit comments managment page

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -1,32 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center pb-3">Comments Managment</h1>
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
-            <table class="table table-primary table-striped ">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Comment</th>
-                    <th scope="col">user</th>
-                    <th scope="col">item</th>
-                    <th scope="col">Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($comments as $comment)
+    <h1 class="text-center pb-3 mt-5">Comments Managment</h1>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-12">
+                <table class="table table-dark table-striped ">
+                    <thead>
                     <tr>
-                        <td>{{$comment->id}}</td>
-                        <td>{{$comment->comment}}</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>delete</td>
+                        <th scope="col">#</th>
+                        <th scope="col">Comment</th>
+                        <th scope="col">user</th>
+                        <th scope="col">item</th>
+                        <th scope="col">Action</th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
-            {{$comments->links()}}
+                    </thead>
+                    <tbody>
+                    @php($counter =1)
+                    @foreach($comments as $comment)
+                        <tr>
+                            <td>{{$counter}}</td>
+                            <td>{{$comment->comment}}</td>
+                            <td>{{$comment->user->name}}</td>
+                            <td>{{$comment->item->item_name}}</td>
+                            <td>
+                                <form action="/Admin/comments/{{$comment->id}}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="btn btn-primary">delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @php($counter++)
+                    @endforeach
+                    </tbody>
+                </table>
+                {{$comments->links()}}
+            </div>
         </div>
+
     </div>
 @endsection
